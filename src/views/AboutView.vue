36319d78de10c360bf4b7d3813b4e5f5cda0fe46
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-18 20:47:58 -0400

    mapbox example && env files

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Philly Philly</h1>
+    <div id="map"></div>
+    <img src="" alt="" />
   </div>
 </template>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MAP_API_KEY;
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/dark-v10", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    console.log(map);
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: darkgreen;
+}
+#map {
+  height: 300px;
+  width: 100%;
+}
+h1 {
+  color: silver;
+}
+</style>
