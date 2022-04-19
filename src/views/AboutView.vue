c4a616d4b040196dfbb6a2e5596e55d9ea329bac
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-18 22:15:01 -0400

    formatted to draw places from backend

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,11 @@
   <div class="about">
     <h1>Philly Philly</h1>
     <div id="map"></div>
-    <img src="" alt="" />
+    <br />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsQdqy6Y7MzAl4Zby6nnQLq-JDKYQMUmn_wA&usqp=CAU"
+      alt=""
+    />
   </div>
 </template>
 
@@ -10,19 +14,38 @@
 /* global mapboxgl */
 export default {
   data: function () {
-    return {};
+    return {
+      places: [],
+    };
   },
   mounted: function () {
-    mapboxgl.accessToken = process.env.VUE_APP_MAP_API_KEY;
-    const map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/dark-v10", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
-    });
-    console.log(map);
+    this.getPlaces();
   },
-  methods: {},
+  methods: {
+    getPlaces() {
+      // make axios
+      this.places = [
+        { lat: 39.9014, lng: -75.1, description: "The Link" },
+        { lat: 39.844, lng: -75.0, description: "My childhood town" },
+      ];
+      this.setMap();
+    },
+    setMap() {
+      mapboxgl.accessToken = process.env.VUE_APP_MAP_API_KEY;
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/dark-v10", // style URL
+        center: [this.places[0].lng, this.places[0].lat], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      this.places.forEach((place) => {
+        // create the popup
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+        const marker = new mapboxgl.Marker().setLngLat([place.lng, place.lat]).setPopup(popup).addTo(map);
+        console.log(map, marker);
+      });
+    },
+  },
 };
 </script>
 
@@ -37,6 +60,7 @@ body {
   width: 100%;
 }
 h1 {
+  font-family: cursive;
   color: silver;
 }
 </style>
